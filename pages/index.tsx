b45f9ed33f30cfa2e4558cf6a1b2aca2a07020f7
Oleksandr Gorpynich <oleksandr.gorpynich@gmail.com>
2023-10-25 10:13:45 -0700

    Add header and create the pages folder

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import '../app/globals.css'
+import Header from '@/components/header'
+
+export default function Index() {
+  return (
+    <main className="min-h-screen bg-white">
+      <Header />
+      Contact Page
+    </main>
+  )
+}
---
+++ pages/signin.tsx
@@ -0,0 +1,11 @@
+import '../app/globals.css'
+import Header from '@/components/header'
+
+export default function Signin() {
+  return (
+    <main className="min-h-screen bg-white">
+      <Header />
+      Signin
+    </main>
+  )
+}
