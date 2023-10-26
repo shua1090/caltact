47779adef6270580f6d082f1c8f88ff827074869
JeremiahLiao <jeremiahjliao@gmail.com>
2023-10-25 21:06:13 -0700

    added major and social media title

--- pages/addpage.tsx
+++ pages/addpage.tsx
@@ -11,7 +11,7 @@ export default function AddPage(){
                 <form>
                     <div className="space-y-12">
                         <div className="border-b border-gray-900/10 pb-12">
-                            <h1 className = "text-base font-semibold leading-7 text-gray-900">Create New Contact</h1>
+                            <h2 className = "text-base font-semibold leading-7 text-gray-900">Create New Contact</h2>
                             <p className = "mt-1 text-sm leading-6 text-gray-600">
                                 Fill out the information of the contact you would like to add below:
                             </p>
@@ -28,7 +28,25 @@ export default function AddPage(){
                                             id="username"
                                             autoComplete="username"
                                             className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-                                            placeholder="janesmith"
+                                            placeholder="Jeremiah Liao"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div className="sm:col-span-4">
+                                    <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                                        Major
+                                    </label>
+                                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                        <input
+                                            type="text"
+                                            name="username"
+                                            id="username"
+                                            autoComplete="username"
+                                            className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                            placeholder="Computer Science"
                                         />
                                     </div>
                                 </div>
@@ -56,6 +74,7 @@ export default function AddPage(){
                                     </div>
                                 </div>
                             </div>
+
                         </div>
 
                         <div className="border-b border-gray-900/10 pb-12">
@@ -186,6 +205,10 @@ export default function AddPage(){
                                 </div>
                                 </div>
                             </div>
+                        </div>
+
+                        <div className="border-b border-gray-900/10 pb-12">
+                            <h2 className = "text-base font-semibold leading-7 text-gray-900">Social Media</h2>
                         </div>
                     </div>
                 </form>
