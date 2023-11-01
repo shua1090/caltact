b778996a8dea20a1599e06a76952ec89cfa2bbc7
JeremiahLiao <jeremiahjliao@gmail.com>
2023-11-01 11:14:50 -0700

    feat: set inputted data to store in contact state

--- pages/addpage.tsx
+++ pages/addpage.tsx
@@ -2,11 +2,14 @@ import "../app/globals.css";
 import Header from "@/components/header";
 import Image from "next/image";
 import pfp  from '../public/pfp.png';
+import { FormEvent, useState } from "react";
 
 interface TextEntryProps {
     label: string;
     id: string;
     placeholder?: string;
+    value: string;
+    changeHandler: (event: FormEvent) => void;
 }
 
 function TextEntry(props: TextEntryProps){
@@ -20,6 +23,8 @@ function TextEntry(props: TextEntryProps){
                     type="text"
                     name={props.id}
                     id={props.id}
+                    value={props.value}
+                    onChange={props.changeHandler}
                     autoComplete={props.id}
                     className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                     placeholder={props.placeholder}
@@ -29,7 +34,108 @@ function TextEntry(props: TextEntryProps){
     );
 };
 
+function handleSubmit(){
+    /* handle form submission */
+}
+
+function handleCancel(){
+    /* handle form cancellation */
+}
+
 export default function AddPage(){
+    const [contact, setContact] = useState({
+        college: "",
+        major: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        address: {
+            country: "",
+            street: "",
+            city: "",
+            region: "",
+            postalCode: ""
+        },
+        social:{
+            facebook: "",
+            instagram: "",
+            twitter: "",
+            linkedin: "",
+            discord: "",
+            github: ""
+        }
+    });
+
+    function handleChange(event: FormEvent){
+        const {name, value} = event.target as HTMLFormElement;
+        switch(name){
+            case "college": {
+                setContact({...contact, college: value});
+                break;
+            }
+            case "major": {
+                setContact({...contact, major: value});
+                break;
+            }
+            case "first-name": {
+                setContact({...contact, firstName: value});
+                break;
+            }
+            case "last-name": {
+                setContact({...contact, lastName: value});
+                break;
+            }
+            case "email": {
+                setContact({...contact, email: value});
+                break;
+            }
+            case "country": {
+                setContact({...contact, address: {...contact.address, country: value}});
+                break;
+            }
+            case "street-address": {
+                setContact({...contact, address: {...contact.address, street: value}});
+                break;
+            }
+            case "city": {
+                setContact({...contact, address: {...contact.address, city: value}});
+                break;
+            }
+            case "region": {
+                setContact({...contact, address: {...contact.address, region: value}});
+                break;
+            }
+            case "postal-code": {
+                setContact({...contact, address: {...contact.address, postalCode: value}});
+                break;
+            }
+            case "facebook": {
+                setContact({...contact, social: {...contact.social, facebook: value}});
+                break;
+            }
+            case "instagram": {
+                setContact({...contact, social: {...contact.social, instagram: value}});
+                break;
+            }
+            case "twitter": {
+                setContact({...contact, social: {...contact.social, twitter: value}});
+                break;
+            }
+            case "linkedin": {
+                setContact({...contact, social: {...contact.social, linkedin: value}});
+                break;
+            }
+            case "discord": {
+                setContact({...contact, social: {...contact.social, discord: value}});
+                break;
+            }
+            case "github": {
+                setContact({...contact, social: {...contact.social, github: value}});
+                break;
+            }
+        }
+    }
+
     return (
         <main className=" min-h-screen bg-white">
             <Header />
@@ -67,13 +173,13 @@ export default function AddPage(){
 
                             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                                 <div className="sm:col-span-4">
-                                    <TextEntry label="College" id="college" placeholder="CENG"/>
+                                    <TextEntry label="College" id="college" placeholder="CENG" value = {contact.college} changeHandler={handleChange}/>
                                 </div>
                             </div>
 
                             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                                 <div className="sm:col-span-4">
-                                    <TextEntry label="Major" id="major" placeholder="Computer Science"/>
+                                    <TextEntry label="Major" id="major" placeholder="Computer Science" value = {contact.major} changeHandler={handleChange}/>
                                 </div>
                             </div>
 
@@ -86,13 +192,15 @@ export default function AddPage(){
                             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                                 <div className="sm:col-span-3">
                                     <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-                                        First name
+                                        First Name
                                     </label>
                                     <div className="mt-2">
                                         <input
                                             type="text"
                                             name="first-name"
                                             id="first-name"
+                                            value = {contact.firstName}
+                                            onChange = {handleChange}
                                             autoComplete="given-name"
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
@@ -108,6 +216,8 @@ export default function AddPage(){
                                             type="text"
                                             name="last-name"
                                             id="last-name"
+                                            value={contact.lastName}
+                                            onChange={handleChange}
                                             autoComplete="family-name"
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
@@ -123,6 +233,8 @@ export default function AddPage(){
                                             id="email"
                                             name="email"
                                             type="email"
+                                            value={contact.email}
+                                            onChange={handleChange}
                                             autoComplete="email"
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
@@ -137,6 +249,8 @@ export default function AddPage(){
                                         <select
                                         id="country"
                                         name="country"
+                                        value={contact.address.country}
+                                        onChange={handleChange}
                                         autoComplete="country-name"
                                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                                         >
@@ -156,6 +270,8 @@ export default function AddPage(){
                                         type="text"
                                         name="street-address"
                                         id="street-address"
+                                        value={contact.address.street}
+                                        onChange={handleChange}
                                         autoComplete="street-address"
                                         className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
@@ -171,6 +287,8 @@ export default function AddPage(){
                                             type="text"
                                             name="city"
                                             id="city"
+                                            value={contact.address.city}
+                                            onChange={handleChange}
                                             autoComplete="address-level2"
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
@@ -186,6 +304,8 @@ export default function AddPage(){
                                             type="text"
                                             name="region"
                                             id="region"
+                                            value={contact.address.region}
+                                            onChange={handleChange}
                                             autoComplete="address-level1"
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
@@ -201,6 +321,8 @@ export default function AddPage(){
                                             type="text"
                                             name="postal-code"
                                             id="postal-code"
+                                            value={contact.address.postalCode}
+                                            onChange={handleChange}
                                             autoComplete="postal-code"
                                             className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                         />
@@ -213,24 +335,36 @@ export default function AddPage(){
                             <h2 className = "text-base font-semibold leading-7 text-gray-900">Social Media</h2>
                             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                                 <div className="sm:col-span-2">
-                                    <TextEntry label="Facebook" id="facebook"/>
+                                    <TextEntry label="Facebook" id="facebook" value={contact.social.facebook} changeHandler={handleChange}/>
                                 </div>
                                 <div className="sm:col-span-2">
-                                    <TextEntry label="Instagram" id="instagram"/>
+                                    <TextEntry label="Instagram" id="instagram" value={contact.social.instagram} changeHandler={handleChange}/>
                                 </div>
                                 <div className="sm:col-span-2">
-                                    <TextEntry label="Twitter" id="twitter"/>
+                                    <TextEntry label="Twitter" id="twitter" value={contact.social.twitter}changeHandler={handleChange}/>
                                 </div>
                                 <div className="sm:col-span-2">
-                                    <TextEntry label="LinkedIn" id="linkedin"/>
+                                    <TextEntry label="LinkedIn" id="linkedin" value={contact.social.linkedin} changeHandler={handleChange}/>
                                 </div>
                                 <div className="sm:col-span-2">
-                                    <TextEntry label="Discord" id="discord"/>
+                                    <TextEntry label="Discord" id="discord" value={contact.social.discord} changeHandler={handleChange}/>
                                 </div>
                                 <div className="sm:col-span-2">
-                                    <TextEntry label="Github" id="github"/>
+                                    <TextEntry label="Github" id="github" value={contact.social.github} changeHandler={handleChange}/>
                                 </div>
                             </div>
+                        </div>
+
+                        <div className="mt-6 flex items-center justify-end gap-x-6">
+                            <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+                                Cancel
+                            </button>   
+                            <button
+                                type="submit"
+                                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Save
+                            </button>
                         </div>
                     </div>
                 </form>
