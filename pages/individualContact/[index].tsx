28ceb70499d994f29b9288839d7fece02a01fb79
Evan Cao <80775580+evayktv@users.noreply.github.com>
2023-11-13 18:00:48 -0800

    added nav bar

--- pages/individualContact/[index].tsx
+++ pages/individualContact/[index].tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import Header from '@/components/header'
 
 const DynamicContactPage = () => {
   const router = useRouter();
@@ -48,34 +49,38 @@ const DynamicContactPage = () => {
   }, [index]);
 
   return (
-    <div className="bg-blue-200 min-h-screen flex items-center justify-center">
-      <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-4 text-green-900">
-          {userData ? `${userData.firstName} ${userData.lastName}` : 'Loading...'}
-        </h1>
-        {userData && (
-          <>
-            <p className="text-gray-700 mb-2">Email: {userData.email}</p>
-            <p className="text-gray-700 mb-2">Phone: {userData.phone}</p>
-            <img
-              src={userData.profilePhotoURL}
-              alt="Profile"
-              className="mb-4 rounded-full w-32 h-32 mx-auto"
-            />
-            <p className="text-gray-700 mb-2">Instagram: {userData.instagram}</p>
-            <p className="text-gray-700 mb-2">Snapchat: {userData.snapChat}</p>
-            <p className="text-gray-700 mb-2">Discord: {userData.discord}</p>
-            <p className="text-gray-700 mb-2">LinkedIn: {userData.linkedin}</p>
-            <div className="text-center mt-4">
-              <Link href="/" className="text-blue-500 underline hover:text-blue-700">
-                Back to Home
-              </Link>
-            </div>
-          </>
-        )}
+    <>
+      <Header />
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="bg-white border border-gray-300 p-6 rounded-lg shadow-md w-full max-w-md">
+          <h1 className="text-2xl font-bold mb-4">
+            {userData ? `${userData.firstName} ${userData.lastName}` : 'Loading...'}
+          </h1>
+          {userData && (
+            <>
+              <p className="text-gray-700 mb-2">Email: {userData.email}</p>
+              <p className="text-gray-700 mb-2">Phone: {userData.phone}</p>
+              <img
+                src={userData.profilePhotoURL}
+                alt="Profile"
+                className="mb-4 rounded-full w-32 h-32 mx-auto"
+              />
+              <p className="text-gray-700 mb-2">Instagram: {userData.instagram}</p>
+              <p className="text-gray-700 mb-2">Snapchat: {userData.snapChat}</p>
+              <p className="text-gray-700 mb-2">Discord: {userData.discord}</p>
+              <p className="text-gray-700 mb-2">LinkedIn: {userData.linkedin}</p>
+              <div className="text-center mt-4">
+                <Link href="/" className="text-blue-500 underline hover:text-blue-700">
+                  Back to Home
+                </Link>
+              </div>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
+  
 };
 
 export default DynamicContactPage;
