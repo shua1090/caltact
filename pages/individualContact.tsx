fc2046c4dc6faeb877c00d9ddafcfd49f4bd9a33
Evan Cao <80775580+evayktv@users.noreply.github.com>
2023-11-13 16:29:46 -0800

    displaying all contact user working

--- pages/individualContact.tsx
+++ pages/individualContact.tsx
@@ -1,35 +1,70 @@
 // pages/individualContact.tsx
-import '../app/globals.css'
-import React from 'react'
-import Link from 'next/link'
-import mockUserData from '../mockUserData.js' // Update the path accordingly
+import { useEffect, useState } from 'react';
 
 const IndividualContact = () => {
+  const [allUsers, setAllUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchAllUsers = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await fetch('/api/getContacts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        },
+        body: JSON.stringify({
+          email: localStorage.getItem('email')
+        })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setAllUsers(data.contacts);
+        setIsLoading(false);
+      } else if (response.status === 401) {
+        // Unauthorized, redirect user to login page
+        window.location.href = '/signin';
+      } else {
+        // Handle other error cases as needed
+        console.error('Failed to fetch users:', response.statusText);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllUsers();
+  }, []); // Run only once when the component mounts
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="bg-blue-200 min-h-screen flex items-center justify-center">
       <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-md">
-        <h1 className="text-2xl font-bold mb-4 text-green-900">
-          {mockUserData.firstName} {mockUserData.lastName}
-        </h1>
-        <p className="text-gray-700 mb-2">Email: {mockUserData.email}</p>
-        <p className="text-gray-700 mb-2">Phone: {mockUserData.phone}</p>
-        <img
-          src={mockUserData.profilePhotoURL}
-          alt="Profile"
-          className="mb-4 rounded-full w-32 h-32 mx-auto"
-        />
-        <p className="text-gray-700 mb-2">Instagram: {mockUserData.instagram}</p>
-        <p className="text-gray-700 mb-2">Snapchat: {mockUserData.snapChat}</p>
-        <p className="text-gray-700 mb-2">Discord: {mockUserData.discord}</p>
-        <p className="text-gray-700 mb-2">LinkedIn: {mockUserData.linkedin}</p>
-        <div className="text-center mt-4">
-          <Link href="/" className="text-blue-500 underline hover:text-blue-700">
-            Back to Home
-          </Link>
-        </div>
+        <h1 className="text-2xl font-bold mb-4 text-green-900">All Users</h1>
+        <ul>
+          {allUsers.map((user, index) => (
+            <li key={index}>
+              {Object.entries(user).map(([key, value]) => (
+                <div key={key}>
+                  <strong>{key}:</strong> {value}
+                </div>
+              ))}
+              <hr />
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default IndividualContact
+export default IndividualContact;
