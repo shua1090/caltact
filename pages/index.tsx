4e9b5e6afc5ba4a376bc0ab405f92ebb49844d21
shua1090 <shynn.lawrence@gmail.com>
2023-11-17 01:30:02 -0800

    Fixed compile Error, removed href

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,68 +1,68 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { use, useEffect, useState } from "react";
-import "../app/globals.css";
-import Header from "@/components/header";
-import type contact from "./api/types/contact";
-import "./index.css";
-import ContactCard from "@/components/contact";
-import { set } from "firebase/database";
-import Link from "next/link";
+import { use, useEffect, useState } from 'react'
+import '../app/globals.css'
+import Header from '@/components/header'
+import type contact from './api/types/contact'
+import './index.css'
+import ContactCard from '@/components/contact'
+import { set } from 'firebase/database'
+import Link from 'next/link'
 
-async function fetchContacts(
+async function fetchContacts (
   setIsLoading: any,
   setContacts: any,
   search: string | undefined,
   important: boolean | undefined
 ) {
-  setIsLoading(true);
-  const response = await fetch("/api/getContacts", {
-    method: "POST",
+  setIsLoading(true)
+  const response = await fetch('/api/getContacts', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`
     },
     body: JSON.stringify({
-      email: localStorage.getItem("email"),
+      email: localStorage.getItem('email'),
       search,
-      important,
-    }),
-  });
+      important
+    })
+  })
   try {
-    const data = await response.json();
-    setContacts(data.contacts);
-    setIsLoading(false);
+    const data = await response.json()
+    setContacts(data.contacts)
+    setIsLoading(false)
   } catch (e) {
     // Redirect user to login page
-    window.location.href = "/signin";
+    window.location.href = '/signin'
   }
 }
 
-export default function Index() {
-  const [isClient, setIsClient] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+export default function Index () {
+  const [isClient, setIsClient] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
   useEffect(() => {
-    setIsClient(true);
-  }, []);
+    setIsClient(true)
+  }, [])
 
-  const [search, setSearch] = useState<string>("");
-  const [important, setImportant] = useState<boolean>(false);
-  const [refetch, setRefetch] = useState<boolean>(true);
-  const [contacts, setContacts] = useState<contact[]>([]);
+  const [search, setSearch] = useState<string>('')
+  const [important, setImportant] = useState<boolean>(false)
+  const [refetch, setRefetch] = useState<boolean>(true)
+  const [contacts, setContacts] = useState<contact[]>([])
 
   useEffect(() => {
     if (refetch && isClient) {
       void fetchContacts(
         setIsLoading,
         setContacts,
-        search === "" ? undefined : search,
+        search === '' ? undefined : search,
         important
-      );
-      setRefetch(false);
+      )
+      setRefetch(false)
     }
-  }, [refetch, isClient]);
+  }, [refetch, isClient])
 
-  function fetchNewContacts() {
-    setRefetch(true);
+  function fetchNewContacts () {
+    setRefetch(true)
   }
   return (
     <main className="min-h-screen bg-white">
@@ -75,21 +75,21 @@ export default function Index() {
             placeholder="Search"
             value={search}
             onChange={(e) => {
-              setSearch(e.target.value);
+              setSearch(e.target.value)
             }}
           />
           <span className="text-gray-500 flex flex-row gap-2">
-            Important{" "}
+            Important{' '}
             <input
               type="checkbox"
               className="text-black"
               style={{
-                width: "1.5rem",
-                height: "1.5rem",
+                width: '1.5rem',
+                height: '1.5rem'
               }}
               value={important.toString()}
               onChange={(e) => {
-                setImportant(e.target.checked);
+                setImportant(e.target.checked)
               }}
             />
           </span>
@@ -105,18 +105,16 @@ export default function Index() {
             ? contacts.map((contact, i) => (
                 <div key={i}>
                   <ContactCard
-                    as="a" // specify 'a' as the 'as' prop
-                    href={`/individualContact/${i}`}
-                    photo={contact.photo ?? ""}
+                    photo={contact.photo ?? ''}
                     firstName={contact.firstName ?? undefined}
                     lastName={contact.lastName ?? undefined}
-                    phoneNumber={contact.phoneNumber ?? ""}
+                    phoneNumber={contact.phoneNumber ?? ''}
                     index={i}
-                    email={contact.email ?? ""}
+                    email={contact.email ?? ''}
                     setIsLoading={setIsLoading}
                   />
                 </div>
-              ))
+            ))
             : !isLoading && (
                 <div className="text-center text-lg font-light">
                   Please login or add more contacts
@@ -130,5 +128,5 @@ export default function Index() {
         </div>
       </div>
     </main>
-  );
+  )
 }
