f550f14cb48804be206a3e4f5bb83ea5f1951897
Shynn Lawrence <61219106+shua1090@users.noreply.github.com>
2023-12-02 20:17:02 -0800

    Check request method in getContact, getUser to get all user settings by email (#84)

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ To lint and run commands, run `npx eslint yourcommand`
 Additionally, for convenience, set up your default formatter to be ESlint
 
 To format, CTRL - SHIT - P -> ESLint: Fix all auto-fixable Problems
+
+Testing backend REST APIs
+curl -X GET localhost:3000/api/getUser -H 'Content-Type: application/json' -d '{"email": "shynnlawrence@gmail.com"}' -v
+
+curl -X POST lGET localhost:3000/api/getUser -H 'POST localhost:3000/api/updateUser -H 'Content-Type: application/json' -d '{"email": "shynnlawrence@gmail.com", "firstName": "Shynn", "lastName": "Lawrence", "session": "c45e71ff-313f-4ddd-a990-774d9c4380ff", "other_info": {"instagram": "shua1008", "discord": "shua1008"}}' -v
--- pages/api/getUser.ts
+++ pages/api/getUser.ts
@@ -1,5 +1,7 @@
+// For use in user profile modification (changing your own details),
+// this will be useful to fill a given user's current details, which
+// they can just edit later on
 import { type NextApiRequest, type NextApiResponse } from 'next'
-import contactManager from '../../database/ContactManager'
 import userManager from '../../database/index'
 import verifyUser from './utils/verifyUser'
 
@@ -23,26 +25,7 @@ export default async function handler (
     if (u === null) {
       res.status(403).json({ message: 'Errored out getting user by passed-in email' })
     } else {
-      // If index isn't a number, convert it to one
-      if (typeof req.body.index !== 'number') {
-        req.body.index = parseInt(req.body.index)
-      }
-      let contacts = await contactManager.getContacts(u.id)
-      if (req.body.index !== null && req.body.index !== undefined) {
-        if (req.body.index <= contacts.length) {
-          res.status(200).json({ contacts: contacts[req.body.index] })
-          return
-        }
-        if (req.body.important === true) {
-          contacts = contacts.filter((c: { important: boolean }) => c.important)
-        }
-        if (req.body.search) {
-          contacts = contacts.filter(
-            (c: { firstName: string, lastName: string }) => (c.firstName.toLowerCase() + ' ' + c.lastName.toLowerCase()).includes(req.body.search.toLowerCase())
-          )
-        }
-        res.status(200).json({ contacts })
-      }
+      res.status(400).json(u)
     }
   } catch (error) {
     console.log(`Error in addContacts: ${error as string}`)
